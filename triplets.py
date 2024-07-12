93f2a086307c46a87d3f46db616a7cbef1b01cfd
Splank1942 <splank1942@gmail.com>
2024-07-12 13:29:20 +0200

    update triplets.py

--- triplets.py
+++ triplets.py
@@ -6,4 +6,8 @@ name3 = input("enter the third name ")
 name3_formatted = name3.strip().capitalize()
 
 print(f"The names are: {name1_formatted}, {name2_formatted}, and {name3_formatted}")
-      
+
+
+x = input("Give me a number to multiply by 10 ").replace(",", "")
+
+print (int(x) * 10)
