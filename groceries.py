0739ad53e3ec55866e0b98432c49a03a82e39597
Splank1942 <splank1942@gmail.com>
2024-07-12 07:15:41 +0200

    Initial commit

---
+++ groceries.py
@@ -0,0 +1,17 @@
+# Eggs: $2, Banana $1, Salad $10, Steak and Rice $30
+
+eggs = 2
+banana = 2
+salad = 10
+steak_and_rice = 30
+
+
+
+print("breakfast cost:")
+print (eggs + banana)
+
+print("lunch cost:")
+print(salad)
+
+print("dinner cost:")
+print(steak_and_rice + eggs)
