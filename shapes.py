161b478846ab823f0cb216889547ce9197f15a8a
Splank1942 <splank1942@gmail.com>
2024-07-12 08:52:36 +0200

    Add Shapes.py

---
+++ shapes.py
@@ -0,0 +1,19 @@
+import math
+
+x = int(input("what is side one of the rectangle? "))
+y = int(input("what is side two of the rectangle? "))
+
+print ("The area of the rectangle is: ")
+print (y * x)
+
+
+r = int(input("What is the Radius of the Circle? "))
+
+print ((r*r) * math.pi)
+
+a = int(input("What is the first side of the triangle? "))
+b = int(input("What is the second side of the triangle? "))
+
+
+print ((a*b)/2)
+
