9fe7ec5658672f451f00bd895ff1967e037132c7
Splank1942 <splank1942@gmail.com>
2024-07-16 14:23:31 +0200

    techville add

--- techville.py
+++ techville.py
@@ -1,18 +1,53 @@
-
-
 def move_forward():
-print("moving forward")
+    print("moving forward")
 
 def turn(direction):
-print(f"turning {direction}")
+    print(f"turning {direction}")
 
 def start_engine():
-print("starting engine")
+    print("starting engine")
 
 def stop_engine():
-print("stopping engine")
+    print("stopping engine")
 
 def follow_roundabout(exit_number):
-print(f"taking exit number {exit_number} from the roundabout")
+    print(f"taking exit number {exit_number} from the roundabout")
 
-exit_number = in 
+print("Start Engine")
+
+destination = input("Where do you want to go? ")
+
+if destination == "library":
+    move_forward()
+    turn("left")
+    print("You have arrived at the library!")
+elif destination == "tech park":
+       move_forward()
+       turn("left")
+       print("You have arrived at the tech park")
+elif destination in ["hospital", "mall", "airport", "university", "stadium"]:
+      move_forward()
+      if destination == "hospital":
+            follow_roundabout(1) 
+            print("You have arrived at the hospital!")
+      elif destination == "mall":
+            follow_roundabout(2)
+            move_forward()
+            turn("right")
+            print("You have arrived at the mall!")
+      elif destination == "airport":
+            follow_roundabout(3)
+            print("You have arrived at the airport!")
+      elif destination == "university" or "stadium":
+            follow_roundabout(4)
+            move_forward()
+            if destination == "university":
+                  turn("left")
+                  print("You have arrived at the university!")
+            else:
+                  turn("right")
+                  print("You have arrived at the stadium!")
+else:
+      print("That is not a valid destination!")
+
+stop_engine()
