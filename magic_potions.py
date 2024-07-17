e05fe37827d82edf61ccdff5a6463f4f877bb299
Splank1942 <splank1942@gmail.com>
2024-07-17 13:10:41 +0200

    Magic Potions

---
+++ magic_potions.py
@@ -0,0 +1,42 @@
+
+def ing():
+    print(f"The Ingredients for {selection} are:")
+    print(potions[selection][0])
+    print(potions[selection][1])
+    print(potions[selection][2])
+
+potions = { 
+    "Invisibility Potion": ["Moonstone", "Dragon Scale", "Fairy Dust"], 
+    "Flying Potion": ["Phoenix Feather", "Troll Tooth", "Mermaid Scale"],
+    "Healing Potion": ["Unicorn Horn", "Elf Hair", "Golden Apple"] 
+      }
+
+print("Welcome to Splank's Potion Emporium!")
+print("Here are the potions we currently have for sale: ")
+for items in potions: 
+    print(items)
+
+selection = input("Which of our magical potions would you like to buy ingredients for? ")
+
+p_ing = potions[selection][0:2]
+
+if selection == "Invisibility Potion":
+    ing()
+elif selection == "Flying Potion":
+    ing()
+elif selection == "Healing Potion":
+    ing()
+else: 
+    print("We do not carry that potion please start over.")
+
+print("Let's buy some Ingredients!")
+
+for item in potions[selection][0:3]:
+        buy = input(f"Would you like to buy {item}? Yes/No? ")
+        if buy == "Yes":
+            print(f"You have bought {item}!")
+        elif buy == "No":
+            print(f"You Have chosen to stop shopping for {selection}!")
+            break
+if buy == "Yes":   
+    print(f"Congratulations you bought all ingredients for {selection}!")
